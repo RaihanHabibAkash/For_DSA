99371c8dd17954bb5d52798e64d84379d30e5cc8
RaihanHabibAkash <raihanakash2003811@gmail.com>
2025-11-20 01:01:18 -0800

    Update the condition

--- Huxn_JS/1. FindMatchedValueFromArray/problem1.js
+++ Huxn_JS/1. FindMatchedValueFromArray/problem1.js
@@ -14,3 +14,8 @@ const findValue = (arr, target) => {
 }
 
 findValue(array, "Akash");
+
+
+// Create a function that will take 2 arguments.
+// first one will take a array and second one will take a target. 
+// Now create the function to find the matched value from the target from array.
