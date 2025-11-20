0f4b2a9706a572ec6a17c59147edf34244a71b8e
RaihanHabibAkash <raihanakash2003811@gmail.com>
2025-11-20 01:03:08 -0800

    For NTG

--- Huxn_JS/1. FindMatchedValueFromArray/problem1.js
+++ Huxn_JS/1. FindMatchedValueFromArray/problem1.js
@@ -1,15 +1,10 @@
 const array = ["Raiha", "Habib", "Akash", "Ash", "RH"];
-let ans = "";
 
 const findValue = (arr, target) => {
     for(let i = 0; i < arr.length; i++) {
         if(arr[i] === target) {
-            ans = arr[i];
-            console.log(`Value ${ans} found in index ${i}`);
+            console.log(`Value is ${arr[i]}`);
         }
-    }
-    if(ans === ""){
-        console.log("Value not found");
     }
 }
 
